509c80411a2395c423c0e83317b95d21dc687d20
Ruben Perez <92794845+Rubpergar@users.noreply.github.com>
2023-05-11 09:45:29 +0200

    Update ProjectController.java

--- src/main/java/aiss/gitminer/controller/ProjectController.java
+++ src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -1,45 +1,119 @@
 package aiss.gitminer.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import aiss.gitminer.model.Project;
-import aiss.gitminer.repository.ProjectRepository;
+import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import aiss.gitminer.repository.ProjectRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import aiss.gitminer.exception.ProjectNotFoundException;
+import aiss.gitminer.model.Project;
 
 @RestController
 @RequestMapping("/gitminer/projects")
 public class ProjectController {
+    
+    @Autowired
+    ProjectRepository repository;
 
-    // TODO: Uncomment after creating the repository
-   @Autowired
-   ProjectRepository projectRepository;
-
-    // GET http://localhost:8080/api/projects
-    @GetMapping
-    public List<Project> findAll() {
-        // TODO: COMPLETE
-        return projectRepository.findAll();
-    }
-
-    // GET http://localhost:8080/api/projects/{id}
-    @GetMapping("/{id}")
-    public Project findOne(@PathVariable String id) {
-        // TODO: COMPLETE
-        return projectRepository.findById(id).get();
-    }
-
-    // POST http://localhost:8080/api/projects
-    @PostMapping
+    // POST Create project --> http://localhost:8080/gitminer/projects
+    @Operation (
+        summary = "Insert a project",
+        description = "Add a new project whose data is passed in the body of the request in JSON format",
+        tags = { "projects", "post" }
+    )
+    @ApiResponses ({
+        @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")}),
+        @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.CREATED)
-    public Project createProject(@RequestBody @Valid Project project) {
-        // TODO: COMPLETE
-        Project _project = projectRepository
-                            .save(new Project(project.getId(), project.getName(), project.getWebUrl(), project.getCommits(), project.getIssues()));
+    @PostMapping
+    public Project create(@Valid @RequestBody Project project) {
+        Project _project = repository
+            .save(new Project(project.getId(), project.getName(), project.getWebUrl(), project.getCommits(), project.getIssues()));
         return _project;
+    }
+    
+    // GET Get all projects --> http://localhost:8080/gitminer/projects
+    @Operation (
+        summary = "Retrieve all projects",
+        description = "Get all projects",
+        tags = { "projects", "get" }
+    )
+    @ApiResponses ({
+        @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")}),
+    })
+    @GetMapping
+    public List<Project> findAll(@RequestParam(required = false) String name,
+                                 @RequestParam(required = false) String order,
+                                 @RequestParam(defaultValue = "0") int page,
+                                 @RequestParam(defaultValue = "10") int size) {
+
+        Pageable paging;
+
+        if(order != null) {
+            if(order.startsWith("-")) {
+                paging = PageRequest.of(page, size, Sort.by(order.substring(1)).descending());
+            } else {
+                paging = PageRequest.of(page, size, Sort.by(order).ascending());
+            }
+        } else {
+            paging = PageRequest.of(page, size); 
+        }
+
+        Page<Project> pageProjects;
+
+        if(name == null) {
+            pageProjects = repository.findAll(paging);
+        } else {
+            pageProjects = repository.findByName(name, paging);
+        }
+
+        return pageProjects.getContent();
+
+    }
+    
+
+    // GET Get project --> http://localhost:8080/gitminer/projects/{id}
+    @Operation (
+        summary = "Retrieve a project by id",
+        description = "Get a project object by specifying its id",
+        tags = { "projects", "get" }
+    )
+    @ApiResponses ({
+        @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")}),
+        @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema())})
+    })
+    @GetMapping("/{id}")
+    public Project findOne(@Parameter(description = "id of the project to be searched") @PathVariable String id) throws ProjectNotFoundException {
+        Optional<Project> project = repository.findById(id);
+        if(!project.isPresent()) {
+            throw new ProjectNotFoundException();
+       }
+        return project.get();
     }
 
 }
