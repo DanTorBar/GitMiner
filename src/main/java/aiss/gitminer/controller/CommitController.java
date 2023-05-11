4bb5d898839d6fc66d27015ebd641dab44cf8444
Ruben Perez <92794845+Rubpergar@users.noreply.github.com>
2023-05-11 09:36:15 +0200

    Update CommitController.java

--- src/main/java/aiss/gitminer/controller/CommitController.java
+++ src/main/java/aiss/gitminer/controller/CommitController.java
@@ -25,23 +25,62 @@ public class CommitController {
     @Autowired
     CommitRepository commitRepository;
 
-    // GET get commit --> http://localhost:8080/gitminer/commits/{id}
-    @GetMapping("/{id}")
-    public Commit findOne(@PathVariable String id) {
-        Optional<Commit> commit = commitRepository.findById(id);
-        return commit.get();
-    }
+    // GET Get all commits --> http://localhost:8080/gitminer/commits?email={emailAddress}
+    @Operation (
+        summary = "Retrieve all commits",
+        description = "Get all commits",
+        tags = { "commits", "get" }
+    )
+    @ApiResponses ({
+        @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Commit.class), mediaType = "application/json")}),
+    })
 
     // GET get commit by email address --> http://localhost:8080/gitminer/commits?email={emailAddress}
     @GetMapping
-    public List<Commit> findCommits(@RequestParam(required = false) String email) {
-        List<Commit> commits = commitRepository.findAll();
-        if (email != null) {
-            commits = commits.stream()
-                                .filter(c -> c.getAuthorEmail().equals(email))
-                                .collect(Collectors.toList());
+    public List<Commit> findCommits(@RequestParam(required = false) String email,
+                                     @RequestParam(required = false) String order,
+                                     @RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "10") int size) {
+        Pageable paging;
+
+        if(order != null) {
+            if(order.startsWith("-")) {
+                paging = PageRequest.of(page, size, Sort.by(order.substring(1)).descending());
+            } else {
+                paging = PageRequest.of(page, size, Sort.by(order).ascending());
+            }
+        } else {
+            paging = PageRequest.of(page, size); 
         }
-        return commits;
+
+        Page<Commit> pageProjects;
+
+        if(title == null) {
+            pageProjects = commitRepository.findAll(paging);
+        } else {
+            pageProjects = commitRepository.findByEmail(email, paging);
+        }
+
+        return pageProjects.getContent();
+    }
+    
+    // GET get commit --> http://localhost:8080/gitminer/commits/{id}
+    @Operation (
+        summary = "Retrieve a commit by id",
+        description = "Get a commit object by specifying its id",
+        tags = { "commit", "get" }
+    )
+    @ApiResponses ({
+        @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Commit.class), mediaType = "application/json")}),
+        @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema())})
+    })
+    @GetMapping("/{id}")
+    public Commit findOne(@Parameter(description = "id of the commit to be searched") @PathVariable String id) throws CommitNotFoundException {
+        Optional<Commit> commit = commitRepository.findById(id);
+        if(!commit.isPresent()) {
+            throw new CommitNotFoundException();
+        }
+        return commit.get();
     }
 
 }
